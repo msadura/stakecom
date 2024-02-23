020cd19eb89a09a56d926a8e78baea81d249d531
Marek Epicode <m.sadura@outlook.com>
2024-02-23 01:46:22 +0100

    update input

--- apps/comstaked/src/components/stake/Stake.tsx
+++ apps/comstaked/src/components/stake/Stake.tsx
@@ -18,14 +18,29 @@ export function Stake() {
             <div className="space-y-1 pt-6">
               <Box justify="between">
                 <Label htmlFor="name" className="text-muted-foreground">
-                  Deposit
+                  Deposit wCom
                 </Label>
                 <Label htmlFor="name" className="text-muted-foreground">
                   Balance:{" "}
                   <span className="font-bold text-foreground">0.0</span>
                 </Label>
               </Box>
-              <Input id="amount" defaultValue="10" />
+              <Box className="relative" align="center">
+                <Box className="absolute left-2.5">
+                  <img src="/commune.svg" alt="wCom" width="24" height="24" />
+                </Box>
+                <Input
+                  id="amount"
+                  defaultValue="10"
+                  className="pl-[40px] pr-[60px]"
+                />
+                <Button
+                  variant="muted"
+                  className="absolute right-[0px] max-w-[60px] rounded-l-none py-1 text-xs"
+                >
+                  Max
+                </Button>
+              </Box>
             </div>
           </CardContent>
           <CardFooter>
@@ -48,7 +63,22 @@ export function Stake() {
                   <span className="font-bold text-foreground">0.0</span>
                 </Label>
               </Box>
-              <Input id="amount" defaultValue="10" />
+              <Box className="relative" align="center">
+                <Box className="absolute left-2.5">
+                  <img src="/commune.svg" alt="wCom" width="24" height="24" />
+                </Box>
+                <Input
+                  id="amount"
+                  defaultValue="10"
+                  className="pl-[40px] pr-[60px]"
+                />
+                <Button
+                  variant="muted"
+                  className="absolute right-[0px] max-w-[60px] rounded-l-none py-1 text-xs"
+                >
+                  Max
+                </Button>
+              </Box>
             </div>
           </CardContent>
           <CardFooter>

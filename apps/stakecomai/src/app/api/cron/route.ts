f79f1584cc60c42e35719d8eb152b48fa9ab7fd5
Marek Epicode <m.sadura@outlook.com>
2024-03-22 14:58:49 +0100

    change cron

--- apps/stakecomai/src/app/api/cron/route.ts
+++ apps/stakecomai/src/app/api/cron/route.ts
@@ -2,18 +2,14 @@ import type { NextRequest } from "next/server";
 import { processEvents } from "~core/events";
 
 export async function GET(req: NextRequest) {
-  console.log(
-    "🔥auth",
-    req.headers.get("Authorization"),
-    process.env.CRON_SECRET,
-  );
-  if (
-    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
-  ) {
-    return new Response("Unauthorized", {
-      status: 401,
-    });
-  }
+  console.log(req.headers.get("Authorization"));
+  // if (
+  //   req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  // ) {
+  //   return new Response("Unauthorized", {
+  //     status: 401,
+  //   });
+  // }
 
   await processEvents();
 

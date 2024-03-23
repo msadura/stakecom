801cf147abb998bdfa03fdbe7dcd2f5853c8c5a6
Marek Epicode <m.sadura@outlook.com>
2024-03-23 23:25:36 +0100

    remove log

--- packages/core/src/events/processEvents.ts
+++ packages/core/src/events/processEvents.ts
@@ -4,7 +4,6 @@ import { pullEvents } from "~core/events/pullEvents";
 let isProcessing = false;
 
 export async function processEvents() {
-  console.log("🔥", "test process reload");
   if (isProcessing) {
     console.log("🚧 Already processing events, skipping.");
     return;

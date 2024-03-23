c1249d2be8a5df561ad70f3f2b0362706f257243
Marek Epicode <m.sadura@outlook.com>
2024-03-23 21:59:40 +0100

    Elysia server processing

---
+++ apps/server/src/index.ts
@@ -0,0 +1,52 @@
+import { cron } from "@elysiajs/cron";
+import { Elysia } from "elysia";
+
+import { processEvents } from "@stakecom/core";
+
+const app = new Elysia()
+  .get("/", () => "I'm ready!")
+  .post(
+    "/process",
+    () => {
+      processEvents().catch((error) => {
+        console.log("🔥 Failed to run process events on request", error);
+      });
+      return { processingStarted: true };
+    },
+    {
+      beforeHandle({ set, request: { headers } }) {
+        const authorization = headers.get("Authorization") || "";
+        const serverSecret = process.env.SERVER_SECRET;
+
+        if (serverSecret && `Bearer ${serverSecret}` !== authorization) {
+          set.status = 401;
+
+          throw new Error("Unauthorized");
+        }
+      },
+    },
+  )
+  .use(
+    cron({
+      name: "processEvents",
+      // run every 1minute
+      pattern: "0 */1 * * * *",
+      run() {
+        processEvents().catch((error) => {
+          console.log("🔥 Failed to run process events in cron", error);
+        });
+      },
+    }),
+  )
+  .on("start", () => {
+    console.log("🚀 Elysia started");
+
+    processEvents().catch((error) => {
+      console.log("🔥 Failed to run process events on start", error);
+    });
+  })
+  .listen(3535);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);

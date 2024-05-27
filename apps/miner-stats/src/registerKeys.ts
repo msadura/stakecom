407e7440e7c0bd18c0cdcd7daf4df623c60b38d6
Marek Epicode <m.sadura@outlook.com>
2024-05-27 22:08:01 +0200

    r

--- apps/miner-stats/src/registerKeys.ts
+++ apps/miner-stats/src/registerKeys.ts
@@ -1,51 +1,11 @@
-// import { getSigner, transfer } from "@stakecom/commune-sdk";
-// import { toAmountValue } from "@stakecom/commune-sdk/utils";
+import { getBurn, getMinStake, getSubnetName } from "@stakecom/commune-sdk";
+import { formatCOMAmount } from "@stakecom/core/formatters";
 
-// import { getFilteredKeys, getKeyByName } from "./getKeys";
+const burnAmount = await getBurn(17);
+console.log("🔥 burn:", formatCOMAmount(burnAmount));
+const subnetName = await getSubnetName(17);
+console.log("🔥 subnet:", subnetName);
+const minStake = await getMinStake(17);
+console.log("🔥 min stake:", formatCOMAmount(minStake));
 
-// const bankName = "epic";
-// const subnet = 17;
-// const ip = "66.94.112.167";
-// const port = "303x";
-
-// export const feedKeys = async (pattern: RegExp) => {
-//   const bankKey = await getKeyByName(bankName);
-//   const filteredKeys = await getFilteredKeys(pattern);
-
-//   console.log("🔥", `Number of keys to feed:`, filteredKeys.length);
-
-//   if (!bankKey) {
-//     throw new Error(`Bank key not found: ${bankName}`);
-//   }
-
-//   for (const key of filteredKeys) {
-//     const { ss58_address, path, mnemonic } = key;
-//     const signer = await getSigner(mnemonic);
-
-//     console.log("🔥", `Registering ${path} to ${bankName}`);
-
-//     try {
-//       // await transfer({
-//       //   signer,
-//       //   recipient: ss58_address,
-//       //   amount: feedAmount,
-//       // });
-
-//       // register on subnet!
-
-//       console.log("✅", `${path} - registered`);
-//     } catch (err) {
-//       console.error("❌", `${path} - error`, err);
-//     }
-//   }
-// };
-
-// feedKeys(/^jottei[2-9]+$/i)
-//   .then(() => {
-//     console.log("🔥 All done");
-//     process.exit();
-//   })
-//   .catch((err) => {
-//     console.error("🔥 Error", err);
-//     process.exit(1);
-//   });
+process.exit();

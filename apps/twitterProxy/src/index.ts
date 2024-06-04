c9b8ac112d4037eb68a0a7ddbb19f03704f61257
Marek Epicode <m.sadura@outlook.com>
2024-06-05 00:29:57 +0200

    f

--- apps/twitterProxy/src/index.ts
+++ apps/twitterProxy/src/index.ts
@@ -98,12 +98,12 @@ const proxyHandler = async (c: Context, url: string, startTime: number) => {
     },
     retry: {
       limit: 10,
-      delay: () => getRandomNumber(500, 2000),
+      delay: () => getRandomNumber(500, 1000),
     },
     hooks: {
       beforeRequest: [
         async () => {
-          await sleep(getRandomNumber(10, 500));
+          await sleep(getRandomNumber(10, 200));
         },
       ],
       beforeRetry: [

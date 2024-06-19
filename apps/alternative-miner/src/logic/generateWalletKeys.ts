929e2f2238977603f04f3b22183bdf9d554f75bb
IShotTheSheriff <enowacki@gmail.com>
2024-06-19 15:53:59 +0200

    Proper generation

--- apps/alternative-miner/src/logic/generateWalletKeys.ts
+++ apps/alternative-miner/src/logic/generateWalletKeys.ts
@@ -4,26 +4,39 @@ import {
   cryptoWaitReady,
   mnemonicGenerate,
   mnemonicToMiniSecret,
-  sr25519PairFromSeed,
+  mnemonicValidate,
 } from "@polkadot/util-crypto";
-
-// create a keyring with some non-default values specified
-// ss58Format specific to Commune
-const keyring = new Keyring({ type: "sr25519", ss58Format: 42 });
 
 export const generateWalletKeys = async () => {
   // Need to be run one time before interacting with the API
   await cryptoWaitReady();
+
+  // create a keyring with some non-default values specified
+  // ss58Format specific to Commune
+  const keyring = new Keyring({ type: "sr25519", ss58Format: 42 });
+
   // Create mnemonic string using BIP39
   const mnemonic = mnemonicGenerate();
+
+  const isValidMnemonic = mnemonicValidate(mnemonic);
+  if (!isValidMnemonic) {
+    throw new Error("Invalid mnemonic generated");
+  }
+
+  // Derive the seed from the mnemonic
   const seed = mnemonicToMiniSecret(mnemonic);
   const hexSeed = u8aToHex(seed);
-  const keyPair = sr25519PairFromSeed(seed);
+
+  // Add a new pair to the keyring using the seed
+  const keyPair = keyring.addFromSeed(seed);
+
+  // The private key in SR25519 consists of both the seed and the public key
+  const privateKey = new Uint8Array([...seed, ...keyPair.publicKey]);
   const address = keyring.encodeAddress(keyPair.publicKey, 42);
 
   return {
     publicKey: u8aToHex(keyPair.publicKey).slice(2),
-    privateKey: u8aToHex(keyPair.secretKey).slice(2),
+    privateKey: u8aToHex(privateKey).slice(2),
     address,
     mnemonic,
     hexSeed: hexSeed.slice(2),

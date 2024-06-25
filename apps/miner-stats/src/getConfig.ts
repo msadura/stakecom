2e387786b5d55d83a624c0b6a70f38478c1bf7ce
Marek Epicode <m.sadura@outlook.com>
2024-06-25 11:17:46 +0300

    update miner config schema

--- apps/miner-stats/src/getConfig.ts
+++ apps/miner-stats/src/getConfig.ts
@@ -7,11 +7,49 @@ const minerSchema = z.object({
   label: z.string().optional(),
 });
 
-const configSchema = z.object({
-  stats: z.array(minerSchema),
-  unstake: z.array(minerSchema),
-  register: z.array(minerSchema.extend({ ipTemplate: z.string() })),
+const minerExtendedSchema = z.object({
+  pattern: patternSchema,
+  label: z.string().optional(),
+  name: z.string(),
+  ipTemplate: z.string(),
 });
+
+const configSchema = z.object({
+  stats: z.array(minerSchema).optional(),
+  unstake: z.array(minerSchema).optional(),
+  register: z.array(minerSchema.extend({ ipTemplate: z.string() })).optional(),
+  miners: z.array(minerExtendedSchema).optional(),
+});
+
+function getConfigFromeExtendedMiners(
+  miners: z.infer<typeof configSchema>["miners"],
+) {
+  if (!miners) {
+    throw new Error("Miners with all information are required");
+  }
+
+  const stats = miners.map((miner) => ({
+    pattern: miner.pattern,
+    label: miner.label,
+  }));
+
+  const unstake = miners.map((miner) => ({
+    pattern: new RegExp(`^${miner.name}$[0-9]`, "i"),
+    label: miner.label,
+  }));
+
+  const register = miners.map((miner) => ({
+    pattern: miner.pattern,
+    label: miner.label,
+    ipTemplate: miner.ipTemplate,
+  }));
+
+  return {
+    stats,
+    unstake,
+    register,
+  };
+}
 
 export async function getConfig() {
   const current = import.meta.dir;
@@ -20,5 +58,9 @@ export async function getConfig() {
 
   const content: unknown = await file.json();
 
-  return configSchema.parse(content);
+  const config = configSchema.parse(content);
+
+  if (config.miners) {
+    return getConfigFromeExtendedMiners(config.miners);
+  }
 }
